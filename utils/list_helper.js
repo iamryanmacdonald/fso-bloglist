dbf83e62919bab49f10579915fb387a5ef7e1ec6
Zenrer <ram_idts@me.com>
2022-06-14 09:55:35 +1000

    finished part 4 exercise 5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,7 +2,23 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const favoriteBlog = (blogs) => {
+  const blog = blogs
+    .map((blog) => {
+      return { ...blog };
+    })
+    .reduce((previous, current) =>
+      previous.likes > current.likes ? previous : current
+    );
+
+  delete blog._id;
+  delete blog.__v;
+  delete blog.url;
+
+  return blog;
+};
+
 const totalLikes = (blogs) =>
   blogs.reduce((previous, current) => (previous += current.likes), 0);
 
-module.exports = { dummy, totalLikes };
+module.exports = { dummy, favoriteBlog, totalLikes };
