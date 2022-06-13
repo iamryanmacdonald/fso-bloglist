9dbf45aada4f71f0f7ed63ec8ed09a7bc2790d21
Zenrer <ram_idts@me.com>
2022-06-14 09:45:12 +1000

    finished part 4 exercise 4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,4 +2,7 @@ const dummy = (blogs) => {
   return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) =>
+  blogs.reduce((previous, current) => (previous += current.likes), 0);
+
+module.exports = { dummy, totalLikes };
