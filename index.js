a9e1bf7f5183bdcbc97746d2cf513fd21e9ecacd
Zenrer <ram_idts@me.com>
2022-06-14 09:33:42 +1000

    finished part 4 exercise 2

--- index.js
+++ index.js
@@ -1,41 +1,21 @@
-require("dotenv").config();
-
-const http = require("http");
-const express = require("express");
-const app = express();
 const cors = require("cors");
+const express = require("express");
+const http = require("http");
 const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
+const blogsRouter = require("./controllers/blogs");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
-const Blog = mongoose.model("Blog", blogSchema);
+mongoose.connect(config.MONGODB_URI);
 
-const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl);
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
