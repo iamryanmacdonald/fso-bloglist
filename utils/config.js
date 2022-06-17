2e6e0a8151973e6c56b38904a980a7d16e80bb99
Zenrer <ram_idts@me.com>
2022-06-17 14:00:17 +1000

    finished part 4 exercise 18

--- utils/config.js
+++ utils/config.js
@@ -5,5 +5,6 @@ const MONGODB_URI =
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI;
 const PORT = process.env.PORT;
+const SECRET = process.env.SECRET;
 
-module.exports = { MONGODB_URI, PORT };
+module.exports = { MONGODB_URI, PORT, SECRET };
