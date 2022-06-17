3c8000b82b0c1201a9f4f85a901d3ffd024592d7
Zenrer <ram_idts@me.com>
2022-06-17 13:47:14 +1000

    finished part 4 exercise 17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 router.delete("/:id", async (request, response) => {
   await Blog.findByIdAndDelete(request.params.id);
@@ -9,15 +10,25 @@ router.delete("/:id", async (request, response) => {
 });
 
 router.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { name: 1, username: 1 });
 
   response.json(blogs);
 });
 
 router.post("/", async (request, response) => {
-  const blog = new Blog({ ...request.body, likes: request.body.likes || 0 });
+  const users = await User.find({});
+  const user = users[0];
+
+  const blog = new Blog({
+    ...request.body,
+    likes: request.body.likes || 0,
+    user: user._id,
+  });
 
   const savedBlog = await blog.save();
+
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
 
   response.status(201).json(savedBlog);
 });
--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,11 @@ const router = require("express").Router();
 const User = require("../models/user");
 
 router.get("/", async (request, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate("blogs", {
+    author: 1,
+    title: 1,
+    url: 1,
+  });
 
   response.json(users);
 });
