3f2fc07ce63582f7f8f3c0ed472a105834f20ca0
Zenrer <ram_idts@me.com>
2022-06-21 14:00:38 +1000

    finished part 5 exercise 11

--- frontend/src/components/Togglable.js
+++ frontend/src/components/Togglable.js
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { forwardRef, useImperativeHandle, useState } from "react";
 
 const Togglable = forwardRef(({ buttonLabel, children }, refs) => {
@@ -25,5 +26,9 @@ const Togglable = forwardRef(({ buttonLabel, children }, refs) => {
     </div>
   );
 });
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+};
 
 export default Togglable;
